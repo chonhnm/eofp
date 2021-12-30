aac8e6bc5b6663ecef2ccbb1a99b461906d1142d
chonhnm <chonhnm@gmail.com>
2021-12-30 09:24:33 +0800

    StateT

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,13 @@
 module Lib where
 
-import Control.Monad.Except (Except, ExceptT, runExceptT, throwError, MonadError (throwError))
+import Control.Monad.Except (Except, ExceptT, MonadError (throwError), runExceptT, throwError)
+import Control.Monad.Reader
+import Control.Monad.State (MonadState (get, put), StateT (runStateT))
 import Data.Functor.Classes (eq1)
 import Data.Functor.Identity (Identity (runIdentity))
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe (fromJust)
-import Control.Monad.Reader
 
 someFunc :: IO ()
 someFunc = print "hello"
@@ -44,6 +45,7 @@ eval0 env (App e1 e2) =
         _ -> error "not function"
 
 exampleExp = Lit 12 `Plus` App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2)
+
 exampleExpErr = Lit 12 `Plus` App (Abs "x" (Var "y")) (Lit 4 `Plus` Lit 2)
 
 type Eval1 a = Identity a
@@ -79,15 +81,15 @@ runEval2 ev = runIdentity (runExceptT ev)
 
 eval2 :: Env -> Expr -> Eval2 Value
 eval2 env (Lit i) = return $ IntVal i
-eval2 env (Var n) = 
-    case Map.lookup n env of 
-      Nothing -> throwError $ "unbound variable: " ++ show n 
-      Just a -> return a 
+eval2 env (Var n) =
+  case Map.lookup n env of
+    Nothing -> throwError $ "unbound variable: " ++ show n
+    Just a -> return a
 eval2 env (Plus e1 e2) =
   do
     val1 <- eval2 env e1
     val2 <- eval2 env e2
-    case (val1, val2) of 
+    case (val1, val2) of
       (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
       _ -> throwError "type error in addition"
 eval2 env (Abs n e) = return $ FunVal env n e
@@ -100,39 +102,90 @@ eval2 env (App e1 e2) =
       _ -> throwError "type error in application"
 
 t2 = runEval2 (eval2 Map.empty exampleExp)
+
 t2b = runEval2 (eval2 Map.empty exampleExpErr)
 
-type Eval3 a = ReaderT Env (ExceptT String Identity) a 
+type Eval3 a = ReaderT Env (ExceptT String Identity) a
 
-runEval3 :: Env -> Eval3 a -> Either String a 
-runEval3 env ev = runIdentity $ runExceptT $ runReaderT ev env 
+runEval3 :: Env -> Eval3 a -> Either String a
+runEval3 env ev = runIdentity $ runExceptT $ runReaderT ev env
 
-eval3 :: Expr -> Eval3 Value 
-eval3 (Lit i) = return $ IntVal i 
-eval3 (Var n) = 
-  do 
-    env <- ask 
-    case Map.lookup n env of 
-      Nothing -> throwError $ "unbound variable: " ++ show n 
-      Just a -> return a 
-eval3 (Plus e1 e2) = 
-  do 
-    val1 <- eval3  e1
-    val2 <- eval3  e2
-    case (val1, val2) of 
+eval3 :: Expr -> Eval3 Value
+eval3 (Lit i) = return $ IntVal i
+eval3 (Var n) =
+  do
+    env <- ask
+    case Map.lookup n env of
+      Nothing -> throwError $ "unbound variable: " ++ show n
+      Just a -> return a
+eval3 (Plus e1 e2) =
+  do
+    val1 <- eval3 e1
+    val2 <- eval3 e2
+    case (val1, val2) of
       (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
-      _ -> throwError "type error in addition"  
-eval3 (Abs n e) = 
-  do 
-    env <- ask 
-    return $ FunVal env n e 
+      _ -> throwError "type error in addition"
+eval3 (Abs n e) =
+  do
+    env <- ask
+    return $ FunVal env n e
 eval3 (App e1 e2) =
-  do  
-   val1 <- eval3 e1 
-   val2 <- eval3 e2 
-   case val1 of 
-     FunVal env' n e -> local (const (Map.insert n val2 env')) (eval3 e)
-     _ -> throwError "type error in application"
+  do
+    val1 <- eval3 e1
+    val2 <- eval3 e2
+    case val1 of
+      FunVal env' n e -> local (const (Map.insert n val2 env')) (eval3 e)
+      _ -> throwError "type error in application"
 
 t3 :: Either String Value
-t3 = runEval3 Map.empty (eval3 exampleExp)     
+t3 = runEval3 Map.empty (eval3 exampleExp)
+
+type Eval4 a = ReaderT Env (ExceptT String (StateT Integer Identity)) a
+
+runEval4 :: Env -> Integer -> Eval4 a -> (Either String a, Integer)
+runEval4 env st m = runIdentity (runStateT (runExceptT (runReaderT m env)) st)
+
+tick :: (Num s, MonadState s m) => m ()
+tick = do
+  st <- get
+  put (st + 1)
+
+eval4 :: Expr -> Eval4 Value 
+eval4 (Lit n) = 
+  do 
+    tick 
+    return $ IntVal n 
+eval4 (Var n) =
+  do 
+    tick 
+    env <- ask 
+    case Map.lookup n env of 
+      Nothing -> throwError $ "unbound variable: " ++ show n
+      Just a -> return a
+eval4 (Plus e1 e2) =
+  do 
+    tick 
+    val1 <- eval4 e1 
+    val2 <- eval4 e2 
+    case (val1, val2) of 
+      (IntVal i1, IntVal i2) -> return $ IntVal $ i1 + i2 
+      _ -> throwError "type error in addition"
+eval4 (Abs n e) =
+  do 
+    tick 
+    env <- ask 
+    return $ FunVal env n e        
+eval4 (App e1 e2) =
+  do 
+    tick 
+    val1 <- eval4 e1 
+    val2 <- eval4 e2 
+    case val1 of 
+      FunVal env' n e -> local (const (Map.insert n val2 env')) (eval4 e)
+      _ -> throwError "type error in application"
+
+t4 :: (Either String Value, Integer)
+t4 = runEval4 Map.empty 0 (eval4 exampleExp)      
+t4a :: (Either String Value, Integer)
+t4a = runEval4 Map.empty 0 (eval4 exampleExpErr)      
+
